274328113b9c5e469eaa11d6140b9038fb24f6d4
ganesh-vellanki <velankiganesh@gmail.com>
2020-09-10 23:53:29 +0530

    feat(grid): Update model barrel file with new exports.

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,3 +1,5 @@
 export { GridColumn } from "./grid-column";
 export { GridConfig } from './grid-config';
 export { PinColumn } from './pin-column';
+export { GridEvents } from './grid-events';
+export { GridInstance } from './grid-instance';
