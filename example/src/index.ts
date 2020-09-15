8ce568bc31d4efc2c328a038ffcfd21a9a3c3e07
ganesh-vellanki <velankiganesh@gmail.com>
2020-09-15 20:25:00 +0530

    feat(example-app): Add home page & # based routing.

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,9 +1,36 @@
 import * as ko from 'knockout';
 import { ComponentRegistry } from './components/component-registry';
+import { RouteInfo, Router } from './router';
 
 class GridAppViewModel {
+    routerHatchId = 'koRouterHatch';
+    router: Router;
+    routes: RouteInfo[];
+    componentView: ko.Observable;
+
     constructor() {
         ComponentRegistry.registerAppComponents(ko);
+        ko.components.register('home', {
+            template: '<grid-intro></grid-intro><dev-engage></dev-engage>'
+        });
+        this.componentView = ko.observable('home');
+        this.initializeRouter();
+    }
+
+    initializeRouter() {
+        this.router = new Router(this.routerHatchId, this.componentView);
+        this.router.addRoutes(this.getRoutes());
+    }
+
+    getRoutes() {
+        return [
+            new RouteInfo(
+                {
+                    path: '/grid-intro',
+                    componentTag: 'grid-intro'
+                }
+            )
+        ];
     }
 }
 
