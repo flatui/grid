2fc2491bc6e1e1256a7ee95137caccb0becdae23
ganesh-vellanki <velankiganesh@gmail.com>
2020-09-21 00:09:51 +0530

    feat(gs-grid): Update grid-config interface.

--- src/interface/grid-config.ts
+++ src/interface/grid-config.ts
@@ -1,2 +1,26 @@
+import { Observable } from "rxjs";
+import { IGridColumn, IGridInstance } from ".";
+
 export interface IGridConfig {
+    /**
+     * Column defs of grid config
+     */
+    columnDefs: IGridColumn[];
+
+    /**
+     * Instance  of grid config
+     */
+    instance: IGridInstance;
+
+    /**
+     * Determines whether instance ready is
+     * @returns true if instance ready 
+     */
+    IsInstanceReady(): boolean;
+
+    /**
+     * Determines whether render is complete.
+     * @returns render complete subscription.
+     */
+    IsRenderComplete(): Observable<boolean>
 }
